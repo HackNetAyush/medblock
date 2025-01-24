5a77fcb2615017727715c526a4ea9cb1ceb193ad
HackNetAyush <swami.ayushing1@gmail.com>
2025-01-25 02:02:35 +0530

    AI Added

--- medblock_backend/src/index.ts
+++ medblock_backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import { Request, Response } from "express";
 import Authroutes from "./routes/Authroutes";
 import dbconnect from "./utils/DbConnect";
+import AiChatRoutes from "./routes/AiRoutes";
 
 const app = express();
 const port = 3000;
@@ -12,6 +13,7 @@ dbconnect();
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1/auth", Authroutes);
+app.use("/api/v1/ai", AiChatRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.json({
